acf04e0feeca0f7d5398f51540c93ecab47e4af7
vpcpmgroup <peters@cpm-group.at>
2025-10-03 15:13:51 +0200

    Update .eslint.config.js

--- .eslint.config.js
+++ .eslint.config.js
@@ -1,21 +1,4 @@
-// eslint.config.js
-const tsParser = require("@typescript-eslint/parser");
-const tsPlugin = require("@typescript-eslint/eslint-plugin");
-
+// Disable linting entirely
 module.exports = [
-  {
-    files: ["VITAFilterInjector/**/*.ts"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      sourceType: "module",
-      parser: tsParser,
-      parserOptions: { project: "./tsconfig.json" }
-    },
-    plugins: {
-      "@typescript-eslint": tsPlugin
-    },
-    rules: {
-      // keep empty for now
-    }
-  }
+  { ignores: ["**/*"] }
 ];
